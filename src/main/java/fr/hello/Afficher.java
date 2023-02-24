8f737e9b7f6fb05398f7212ddffe35727c3f4112
dusertest1 <125673454+dusertest1@users.noreply.github.com>
2023-02-24 13:30:23 +0100

    Update Afficher.java

--- src/main/java/fr/hello/Afficher.java
+++ src/main/java/fr/hello/Afficher.java
@@ -2,13 +2,6 @@ package fr.hello;
 
 public class Afficher {
 
-	/**
-	 *	 * @param message
-	 *					display message
-	 *vv
-	 */
-
-	
  public void display(String message)
 	{
 	System.out.println(message) ;
